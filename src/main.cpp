91ef184e308fc12a8c36ad37d6481d8ac3849a59
d.shipilov <d.shipilov@vk.team>
2022-12-30 21:51:21 +0300

    add submodule docker client

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include "docker.h"
+
+int main() {
+    Docker client;
+    auto images = client.list_images();
+    for (auto &image: images.GetObject()) {
+        std::cout << jsonToString(image.name) << ':';
+        std::cout << jsonToString(image.value) << std::endl;
+    }
+
+    return 0;
+}
