b27db369f37f72ece20e154647444ff1b0deed8e
d.shipilov <d.shipilov@vk.team>
2023-01-03 12:44:46 +0300

    add restinio as sources

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "docker.h"
 #include "unifex/execute.hpp"
+#include "restinio/all.hpp"
 
 int main() {
     Docker client;
@@ -9,5 +10,11 @@ int main() {
         std::cout << jsonToString(image.value) << std::endl;
     }
 
+    restinio::run(restinio::on_thread_pool(4)
+                          .port(8050)
+                          .address("0.0.0.0")
+                          .request_handler([](const restinio::request_handle_t &req) {
+                              return restinio::request_accepted();
+                          }));
     return 0;
 }
