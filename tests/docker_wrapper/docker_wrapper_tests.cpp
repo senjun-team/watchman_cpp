5376f3dcffdba45a32dd76b911df69e4ecafc367
d.shipilov <d.shipilov@vk.team>
2023-01-18 12:42:21 +0300

    continue tests

--- tests/docker_wrapper/docker_wrapper_tests.cpp
+++ tests/docker_wrapper/docker_wrapper_tests.cpp
@@ -4,10 +4,11 @@
 
 using namespace watchman;
 
+std::string const kPythonImage = "senjun_courses_python";
+
 TEST(DockerWrapper, run_kill_delete) {
     for (size_t index = 0; index < 10; ++index) {
-        std::string imageName = "senjun_courses_python";
-        DockerRunParams params{.image = std::move(imageName), .tty = true, .memoryLimit = 7000000};
+        DockerRunParams params{.image = kPythonImage, .tty = true, .memoryLimit = 7000000};
         DockerWrapper dockerWrapper;
         std::string const id = dockerWrapper.run(std::move(params));
         ASSERT_TRUE(!id.empty());
@@ -18,11 +19,15 @@ TEST(DockerWrapper, run_kill_delete) {
 
 TEST(DockerWrapper, exec) {
     DockerWrapper dockerWrapper;
+    //    DockerRunParams params{.image = kPythonImage, .tty = true, .memoryLimit = 7000000};
+    //    std::string const id = dockerWrapper.run(std::move(params));
+    //
+    //    std::vector<std::string> args{"sh", "run.sh", "m.py"};
 
-    for (size_t index = 0; index < 10; ++index) {
-        std::string id = "c67c6d15ce9b";
-        std::vector<std::string> args{"sh", "run.sh", "m.py"};
-        auto result = dockerWrapper.exec({std::move(args), std::move(id)});
-        ASSERT_TRUE(result.exitCode == 0);
-    }
+    std::string id = "8770ff79498e";
+    std::vector<std::string> args{"touch", "m.py"};
+    auto result = dockerWrapper.exec({args, id});
+    args = {"echo", "m.py"};  // find combination for set text
+    result = dockerWrapper.exec({std::move(args), std::move(id)});
+    ASSERT_TRUE(result.exitCode == 0);
 }
