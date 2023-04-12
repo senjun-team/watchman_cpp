bb118734ee9641d02f294d823745341a82c46768
d.shipilov <d.shipilov@vk.team>
2023-04-12 07:49:54 +0300

    WATCHMAN-CPP: find config at /etc and near the binary before starting server

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,38 @@
+#include "common/logging.hpp"
 #include "server/server.hpp"
 
+#include <filesystem>
 #include <string>
 
+namespace {
+
+std::string const kConfig = "watchman_config.json";
+
+// we will find config at /etc and near the binary
+std::string findConfig() {
+    bool const existsNearBinary = std::filesystem::exists(kConfig);
+    if (existsNearBinary) {
+        return kConfig;
+    }
+
+    std::string etcConfig = fmt::format("/etc/{}", kConfig);
+    bool existsAtEtc = std::filesystem::exists(etcConfig);
+    if (existsAtEtc) {
+        return etcConfig;
+    }
+
+    return {};
+}
+}  // namespace
+
 int main() noexcept {
-    std::string const kConfig = "config.json";
-    watchman::Server server(kDefaultHost, kConfig);
+    auto const configPath = findConfig();
+    if (configPath.empty()) {
+        watchman::Log::error("{} not found at /etc or near the binary", kConfig);
+        return -1;
+    }
+
+    watchman::Server server(kDefaultHost, configPath);
     server.start();
     return 0;
 }
