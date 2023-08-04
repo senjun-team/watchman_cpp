f5a5859506c01667a9118d51395cbf9eef890aba
Dmitrii <shipilov.dmitry.valer@gmail.com>
2023-08-04 09:11:41 +0300

    remove files and directories on request process

--- images/python/run.sh
+++ images/python/run.sh
@@ -1,4 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# remove all regular files except:
+# * run.sh
+# * task
+# * task_tests
+me=$(basename "$0")
+find . \
+    -type f \
+    -not -name $me \
+    -not -name "task" \
+    -not -name "task_tests" -delete \
+
+# remove all directories from current directory
+find -mindepth 1 -maxdepth 1 -type d -print0 | xargs -r0 rm -R
 
 timeout 10s python $1
 echo $?
