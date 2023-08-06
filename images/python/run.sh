96cedf502baf7b967a4c48766cde67b57d43524f
Dmitrii <shipilov.dmitry.valer@gmail.com>
2023-08-06 17:19:22 +0300

    clear /home/code_runner for rust

--- images/python/run.sh
+++ images/python/run.sh
@@ -1,18 +1,7 @@
 #!/usr/bin/env bash
 
-# remove all regular files except:
-# * run.sh
-# * task
-# * task_tests
-me=$(basename "$0")
-find . \
-    -type f \
-    -not -name $me \
-    -not -name "task" \
-    -not -name "task_tests" -delete \
-
-# remove all directories from current directory
-find -mindepth 1 -maxdepth 1 -type d -print0 | xargs -r0 rm -R
+# clear previous container use
+rm -rf /home/code_runner > /dev/null 2>&1
 
 timeout 10s python $1
 echo $?
