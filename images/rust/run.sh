cd7ad80668f10e8e2fb2e5bccd801c3a7bc2b7f0
Olga Khlopkova <mesozoic.drones@gmail.com>
2023-08-29 15:33:49 +0300

    Parse arguments in run.sh

--- images/rust/run.sh
+++ images/rust/run.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+# parse flags - single letters prefixed fith hyphen before each argument
+# example: sh run.sh -c never -j 4
+while getopts c:j: flag
+do
+    case "${flag}" in
+        c) color=${OPTARG};;
+        j) jobs=${OPTARG};;
+    esac
+done
+
+
+# build 'cargo_run' command options
+# example: --color never --jobs 5
+cargo_run_opts=""
+
+if [ ! -z "$color" ];
+then
+    cargo_run_opts="${cargo_run_opts} --color ${color}"
+fi
+
+if [ ! -z "$jobs" ]
+then
+    cargo_run_opts="${cargo_run_opts} --jobs ${jobs}"
+fi
+
+
 # clear previous container use
 rm -rf /home/code_runner > /dev/null 2>&1
 
@@ -13,5 +39,5 @@ f="$(basename -- $1)"
 # TODO think about cp, should be mv!
 cp -f src/"$f" "src/main.rs"
 
-timeout 10s cargo clean && cargo run -r -q --offline
+timeout 10s cargo clean && cargo run --release --quiet --offline $cargo_run_opts
 echo $?
