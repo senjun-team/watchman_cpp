3bb92467b422de5d15bed33bba3c334a10b3f014
Olga Khlopkova <mesozoic.drones@gmail.com>
2023-08-31 10:09:55 +0300

    Pass args to python container

--- images/python/run.sh
+++ images/python/run.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
+# parse flags - single letters prefixed fith hyphen before each argument
+# example: sh run.sh -f task
+while getopts c:j:f: flag
+do
+    case "${flag}" in
+        c) color=${OPTARG};;
+        j) jobs=${OPTARG};;
+        f) file=${OPTARG};;
+    esac
+done
+
 # clear previous container use
 rm -rf /home/code_runner > /dev/null 2>&1
 
-timeout 10s python $1
+timeout 10s python $file
 echo $?
