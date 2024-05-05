bec93449d95ffe97d792446a0db1f9b07976630c
Olga Khlopkova <mesozoic.drones@gmail.com>
2024-05-05 12:47:56 +0000

    Haskell image

---
+++ images/haskell/run.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# parse flags - single letters prefixed fith hyphen before each argument
+# example: sh run.sh -c never -j 4
+while getopts c:j:f:v: flag
+do
+    case "${flag}" in
+        c) color=${OPTARG};;
+        j) jobs=${OPTARG};;
+        f) file=${OPTARG};;
+        v) task_type=${OPTARG};;
+    esac
+done
+
+
+# clear previous container use
+# rm -rf /home/code_runner > /dev/null 2>&1
+
+# run user code
+f="$(basename -- $file)"
+
+cp /home/code_runner/$f /home/code_runner/user-code/app/Main.hs
+
+# go to /home/code_runner/user_code for stack compiling and running
+cd /home/code_runner/user-code
+
+f_capture="/tmp/capture.txt"
+
+if ! ( timeout 10s stack build --verbosity warn --ghc-options -fno-warn-missing-export-lists  && stack exec user-code-exe  | tee $f_capture ); then
+   echo user_solution_error_f936a25e
+   exit
+fi
+echo user_code_ok_f936a25e
+
+
+# run tests
+timeout 10s python3 "/home/code_runner/${f}_tests" $f_capture
+if [ "$?" -ne 0 ]; then
+    echo tests_cases_error_f936a25e
+    exit
+fi
+
+echo user_solution_ok_f936a25e
