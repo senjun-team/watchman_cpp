8dd0da934aa7eaa0b29fbf715e4aecb38df4c02c
Dmitry Shipilov <shipilovdmitryvaler@gmail.com>
2024-07-12 12:04:18 +0000

    Cpp image

---
+++ images/playground/cpp/run.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# parse flags - single letters prefixed fith hyphen before each argument
+# example: sh run.sh -f task
+while getopts c:j:f:t:v: flag
+do
+    case "${flag}" in
+        c) color=${OPTARG};;
+        j) jobs=${OPTARG};;
+        f) file=${OPTARG};;
+        t) type_check=${OPTARG};;
+        v) task_type=${OPTARG};;
+    esac
+done
+
+f="$(basename -- $file)"
+
+# clear previous container use
+rm -rf /home/code_runner > /dev/null 2>&1
+cp $f main.cpp # otherwise impossible to compile
+
+cd /home/code_runner
+
+executable="main"
+
+if ! ( timeout 10s g++ -o $executable main.cpp && ./$executable ); then
+   echo user_solution_error_f936a25e
+   exit
+fi
+echo user_solution_ok_f936a25e
---
+++ tests/service/courses_tests.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+
+#include "common.hpp"
+#include "core/service.hpp"
+
+TEST(Coursess, C_plus_plous) {
+    watchman::Service service(watchman::readConfig(kParams.config));
+    std::string containerType = "cpp_check";
+    std::string sourceCode =
+        "#include <iostream>\nusing namespace std;\n int main(){\n\tcout<<\"Hello, world\";}";
+    std::string testingCode =
+        "#include <iostream>\nusing namespace std;\n int main(){\n\tcout<<\"Hello, world\";}";
+
+    watchman::RunTaskParams const params{{std::move(containerType), std::move(sourceCode), {}},
+                                         std::move(testingCode)};
+    watchman::Response response = service.runTask(params);
+    ASSERT_TRUE(response.sourceCode == 0);
+    ASSERT_EQ(response.output, "Hello, world");
+    ASSERT_EQ(response.testsOutput.value(), "Hello, world");
+    ASSERT_TRUE(!response.output.empty());
+}
