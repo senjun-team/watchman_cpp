ccda51e3f0c3f3e698e3d6ca7fd199272c0c0ea0
Dmitrii <shipilov.dmitry.valer@gmail.com>
2024-07-16 22:50:16 +0300

    parse json to Folder structure

--- tests/parser/parser_tests.cpp
+++ tests/parser/parser_tests.cpp
@@ -3,6 +3,8 @@
 #include "core/parser.hpp"
 #include "core/service.hpp"
 
+#include <fstream>
+
 TEST(Parser, emptyTestString) {
     watchman::Response response;
     response.output = "lalala";
@@ -19,4 +21,27 @@ TEST(Parser, notEmptyTestString) {
     response.testsOutput = "not null";
 
     ASSERT_NE(makeJsonCourse(std::move(response)), std::string{""});
+}
+
+TEST(Parser, FoldersParser) {
+    std::string assetName = std::string{TEST_DATA_DIR} + "files_structure.json";
+    std::ifstream file(assetName);
+    std::stringstream json;
+    json << file.rdbuf();
+
+    watchman::Folder rootFolder = watchman::fillFolder(json.str());
+    ASSERT_EQ(rootFolder.name, "watchman_cpp");
+    ASSERT_EQ(rootFolder.files.size(), 3);
+    ASSERT_EQ(rootFolder.folders.size(), 1);
+
+    auto const & src = rootFolder.folders.back();
+    ASSERT_EQ(src.name, "src");
+    ASSERT_EQ(src.files.size(), 2);
+    ASSERT_EQ(src.folders.size(), 1);
+
+    auto const & core = src.folders.back();
+
+    ASSERT_EQ(core.name, "core");
+    ASSERT_EQ(core.files.size(), 4);
+    ASSERT_TRUE(core.folders.empty());
 }
