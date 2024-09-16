4953dd0944e083fb7bf12d4fa85ea4a614f5771c
Dmitrii <shipilov.dmitry.valer@gmail.com>
2024-09-16 22:25:04 +0300

    better

--- src/common/tar_to_stream.hpp
+++ src/common/tar_to_stream.hpp
@@ -17,11 +17,11 @@ template<typename T>
 void tar_to_stream(T & stream,                    /// stream to write to, e.g. ostream or ofstream
                    std::string const & filename,  /// name of the file to write
                    char const * data,             /// pointer to the data in this archive segment
-                   size_t size,                   /// size of the data
-                   uint64_t mtime = 0u,           /// file modification time, in seconds since epoch
+                   uint64_t size,                 /// size of the data
+                   uint64_t mtime = 0,            /// file modification time, in seconds since epoch
                    std::string filemode = "644",  /// file mode
-                   unsigned int uid = 0u,         /// file owner user ID
-                   unsigned int gid = 0u,         /// file owner group ID
+                   uint32_t uid = 0u,             /// file owner user ID
+                   uint32_t gid = 0u,             /// file owner group ID
                    std::string const & uname = "root",    /// file owner username
                    std::string const & gname = "root") {  /// file owner group name
     /// Read a "file" in memory, and write it as a TAR archive to the stream
@@ -52,7 +52,7 @@ void tar_to_stream(T & stream,                    /// stream to write to, e.g. o
         char padding[12] = {};  // 500    padding to reach 512 block size
     } header;                   // 512
 
-    filemode.insert(filemode.begin(), 7 - filemode.length(), '0');  // zero-pad the file mode
+    filemode.insert(filemode.begin(), 7 - filemode.size(), '0');  // zero-pad the file mode
 
     std::copy(filename.begin(), filename.begin() + sizeof(header.name) - 1, header.name);
     std::copy(filemode.begin(), filemode.begin() + sizeof(header.mode) - 1, header.mode);
@@ -64,38 +64,27 @@ void tar_to_stream(T & stream,                    /// stream to write to, e.g. o
         header.typeflag = '5';
     }
 
+    auto const getStringStream = [](auto && value, uint32_t size) -> std::stringstream {
+        std::stringstream ss;
+        // use convertion to octal intentionally
+        ss << std::oct << std::setw(static_cast<int>(size)) << std::setfill('0') << value;
+        return ss;
+    };
+
     if (size != 0) {
         // Use a stringstream to convert the size to an octal string
-        std::stringstream ss;
-        ss << std::oct << std::setw(sizeof(header.size) - 1) << std::setfill('0') << size;
-        ss >> header.size;
-
-        // We don't need to worry about mtime and checksum, as they are not used by tar.
-        // However, we will add them here in case you need to use them for something else.
-
-        // Use a stringstream to convert the modification time to an octal string
-        std::stringstream ss_mtime;
-        ss_mtime << std::oct << std::setw(sizeof(header.mtime) - 1) << std::setfill('0') << mtime;
-        ss_mtime >> header.mtime;
-
-        std::stringstream ss_uid;
-        ss_uid << std::oct << std::setw(sizeof(header.uid) - 1) << std::setfill('0') << uid;
-        ss_uid >> header.uid;
-
-        std::stringstream ss_gid;
-        ss_gid << std::oct << std::setw(sizeof(header.gid) - 1) << std::setfill('0') << gid;
-        ss_gid >> header.gid;
+        getStringStream(size, sizeof(header.size) - 1) >> header.size;
+        getStringStream(mtime, sizeof(header.mtime) - 1) >> header.mtime;
+        getStringStream(uid, sizeof(header.uid) - 1) >> header.uid;
+        getStringStream(gid, sizeof(header.gid) - 1) >> header.gid;
 
         // Calculate the checksum, as it is used by tar
-        unsigned int checksum_value = 0;
+        uint32_t checksum_value = 0;
         for (uint32_t i = 0; i != sizeof(header); ++i) {
             checksum_value += reinterpret_cast<uint8_t *>(&header)[i];
         }
 
-        std::stringstream ss_chksum;
-        ss_chksum << std::oct << std::setw(sizeof(header.chksum) - 2) << std::setfill('0')
-                  << checksum_value;
-        ss_chksum >> header.chksum;
+        getStringStream(checksum_value, sizeof(header.chksum) - 2) >> header.chksum;
     } else {
         // If the size is 0, then we need to fill in a size of 00000000000
         for (uint32_t i = 0; i < sizeof(header.size); ++i) {
