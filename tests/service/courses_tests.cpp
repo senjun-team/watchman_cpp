0112397a1b0424c490c4d8467b20d5d5891a9094
Dmitrii <shipilov.dmitry.valer@gmail.com>
2025-01-02 10:46:35 +0300

    SENJUN-1: test for haskell task

--- tests/service/courses_tests.cpp
+++ tests/service/courses_tests.cpp
@@ -65,3 +65,21 @@ TEST(Courses, Rust) {
     ASSERT_EQ(response.output, "Hello world!\r\n");
     ASSERT_EQ(response.testsOutput.value(), "Hello world!\r\n");
 }
+
+TEST(Courses, Haskell) {
+    watchman::Service service(watchman::readConfig(kParams.config));
+    watchman::TaskLauncherType taskType = watchman::TaskLauncherType::HASKELL_COURSE;
+    std::string sourceCode =
+        "{- You can edit and run this code. -}\n\nmodule Main where\n\nmain :: IO ()\nmain = putStrLn (\"Hello world!\")\n";
+    std::string testingCode = "print(\"Hello world!\")";  // test written in python
+    watchman::CourseTaskParams const params{{
+                                                taskType,
+                                                {"-v", "code"},
+                                            },
+                                            std::move(sourceCode),
+                                            std::move(testingCode)};
+    watchman::Response response = service.runTask(params);
+    ASSERT_EQ(response.sourceCode, 0);
+    ASSERT_EQ(response.output, "Hello world!\r\n");
+    ASSERT_EQ(response.testsOutput.value(), "Hello world!\r\n");
+}
